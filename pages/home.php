b4a338f79999dd45d6015930377759af17112045
MilanKV <milanlemonh@gmail.com>
2023-06-08 10:33:40 +0200

    Add ActionToolbar dropdown buttons upload,create

--- pages/home.php
+++ pages/home.php
@@ -16,10 +16,33 @@
     </ul>
 </div>
 <!-- ActionToolbar  -->
-
 <div class="nav_toolbar">
     <div class="action_toolbar">
-        <button class="btn">Upload <i class='bx bx-chevron-down'></i></button>
-        <button class="btn">Create <i class='bx bx-chevron-down'></i></button>
+        <div class="select">
+            <button class="btn">Upload <i class='bx bx-chevron-down'></i></button>
+            <ul class="sub-menu hide">
+                <li>
+                    <i class='bx bx-folder' ></i>
+                    <a href="#">Folder</a>
+                </li>
+                <li>
+                    <i class='bx bx-file-blank'></i>
+                    <a href="#">File</a>
+                </li>
+            </ul>
+        </div>
+        <div class="select">
+            <button class="btn">Create <i class='bx bx-chevron-down'></i></button>
+            <ul class="sub-menu hide">
+                <li>
+                    <i class='bx bx-folder' ></i>
+                    <a href="#">Folder</a>
+                </li>
+                <li>
+                    <i class='bx bx-file-blank'></i>
+                    <a href="#">File</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </div>
